1b4c904f38ff1f929ca6197f8ebe5c8df8a28426
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-18 15:38:38 -0300

    map

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,56 +1,56 @@
 const openPage = (pageName, elmnt, bgcolor, color) => {
   let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName('tabcontent');
+  if (pageName === 'map') {
+    googleMaps();
+  }
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = 'none';
   }
   tablinks = document.getElementsByClassName('tablink');
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = '#F29F05';
+    tablinks[i].style.backgroundColor = '';
+    tablinks[i].style.color = 'red';
   }
   document.getElementById(pageName).style.display = 'block';
-  elmnt.style.backgroundColor = '#F27405';
+  elmnt.style.backgroundColor = bgcolor;
+  elmnt.style.color = color;
 }
 
 // Get the element with id='defaultOpen' and click on it
 document.getElementById('defaultOpen').click();
 
-function placeOnTheMap(place) {
-  const markers = [];
-  let center = {
-    lat: 41.386230,
-    lng: 2.174980
-  };
+const googleMaps = () => {
+  console.log("print map")
+  const stores = [{
+    lat: -23.5742546,
+    lng: -46.677624
+  }, {
+    lat: -23.553994,
+    lng: -46.6600004
+  },{
+    lat: -23.6020718,
+    lng: -46.6742054
+  },{
+    lat: -23.5498462,
+    lng: -46.7088672
+  }];
+  var myLatlng = new google.maps.LatLng(-23.5742546, -46.677624);
+
   const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
-    center: center
+    center: myLatlng,
+  });
+  let markers = stores.map((location, i) => {
+    return new google.maps.Marker({
+      position: location,
+      map: map,
+      label: "GF"
+    });
   });
 
-  // let center = {
-  //   lat: 41.386230,
-  //   lng: 2.174980
-  // };
-}
 
-const getPlaces = () => {
-  axios.get("/api")
-    .then(response => {
-      placeOnTheMap(response.data);
-      console.log(response)
-    })
-    .catch(error => {
-      console.log(error);
-    })
-}
+  markers.push(map);
+  console.log(markers)
 
-let ctx = document.querySelector('#canvas');
-
-// let myGraph = new Chart(ctx, {
-//   type: 'bar',
-//   data: {
-//     labels: [1,2,3,4,5,6,7];
-//     datasets: [{
-//       // label: 
-//     }]
-//   }
-// });
+};
